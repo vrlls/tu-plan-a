d94dfda546b154a6ce040b63b31525f8d4f46ea5
Sebastian Viloria R <30907860+vrlls@users.noreply.github.com>
2022-04-16 17:23:33 -0500

    Add category model and controller with all tests and serializer (#1)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,9 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -63,6 +66,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include ApiHelpers
 end
 
 Shoulda::Matchers.configure do |config|
