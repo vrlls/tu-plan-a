a3013da7c86ceaf45158c5cc9dd77ce9ef6e3883
Sebastian Viloria R <30907860+vrlls@users.noreply.github.com>
2022-11-21 16:04:08 -0500

    Add images to the event serializer (#26)

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -4,4 +4,14 @@ class EventSerializer
   include JSONAPI::Serializer
   attributes :name, :location, :description
   belongs_to :category
+
+  attribute :cover_url do |object|
+    Rails.application.routes.url_helpers.rails_blob_url(object.cover, only_path: true) if object.cover.attached?
+  end
+
+  attribute :images_urls do |object|
+    object.images.map do |image|
+      Rails.application.routes.url_helpers.rails_blob_url(image, only_path: true) if object.images.attached?
+    end
+  end
 end
